442b97c9b9139311c0ea66fe0bc7c64452e0332f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-06-09 15:02:31 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,40 @@
-export default function createStore(reducer) {
-  // add your code here
+export let state;
+
+export const managePets = (state = {
+  pets: [],
+}, action) => {
+  switch(action.type) {
+    case "ADD_PET":
+      return {
+        ...state,
+        pets: [
+          ...state.pets,
+          action.pet
+        ]
+      };
+    case "REMOVE_PET":
+      const petIndex = state.pets.findIndex(pet => pet.id === action.id);
+      return {
+        ...state,
+        pets: [
+          ...state.pets.slice(0, petIndex),
+          ...state.pets.slice(petIndex + 1)
+        ]
+      };
+    default:
+      return state;
+  }
 }
 
-function render() {
-  const container = document.getElementById('container');
+export const dispatch = (action) => {
+  state = managePets(state, action);
+  render();
+};
+
+export const render = () => {
+  let container = document.getElementById('container');
+  let petsList = state.pets.map((pet) => {
+    return `<li>${pet.name}</li>`;
+  }).join(' ');
+  container.innerHTML = `<ul>${petsList}</ul>`;
 }
